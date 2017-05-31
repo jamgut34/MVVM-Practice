a9af5b98a9e3b7a5fdc89d2b3049f5dd292fe239
jamgut34 <jamgut34@gmail.com>
2017-05-30 21:57:35 -0500

    base

---
+++ MVVM Practice/MVVM Practice/Models/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Practice.Models
+{
+    class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        private string fullName;
+        public string FullName
+        {
+            get { return firstName + " " + lastName; }
+            set
+            {
+                fullName = firstName + " " + lastName;
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
